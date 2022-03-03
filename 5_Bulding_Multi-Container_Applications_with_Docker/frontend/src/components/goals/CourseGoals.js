980b7d948696cba0e32d034e3502ed97d196d757
Seong-Jin Kim <sys.ryan0902@gmail.com>
2022-03-04 01:11:16 +0900

    enhance frontend container
    - add .dockerignore
    - add volumes
    - add bind mounts

--- 5_Bulding_Multi-Container_Applications_with_Docker/frontend/src/components/goals/CourseGoals.js
+++ 5_Bulding_Multi-Container_Applications_with_Docker/frontend/src/components/goals/CourseGoals.js
@@ -1,24 +1,20 @@
-import React from 'react';
+import React from "react";
 
-import './CourseGoals.css';
-import Card from '../UI/Card';
-import GoalItem from './GoalItem';
+import "./CourseGoals.css";
+import Card from "../UI/Card";
+import GoalItem from "./GoalItem";
 
 function CourseGoals(props) {
   const hasNoGoals = !props.goals || props.goals.length === 0;
 
   return (
-    <section id='course-goals'>
+    <section id="course-goals">
       <Card>
+        <h2>Your Goals</h2>
         {hasNoGoals && <h2>No goals found. Start adding some!</h2>}
         <ul>
           {props.goals.map((goal) => (
-            <GoalItem
-              key={goal.id}
-              id={goal.id}
-              text={goal.text}
-              onDelete={props.onDeleteGoal}
-            />
+            <GoalItem key={goal.id} id={goal.id} text={goal.text} onDelete={props.onDeleteGoal} />
           ))}
         </ul>
       </Card>
